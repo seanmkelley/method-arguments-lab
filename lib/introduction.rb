274799f24d180110a8b4675d4a9782e23cd46083
seankelley <smkelley.1997@gmail.com>
2020-12-09 15:03:13 -0500

    commit tree 918c692de8109f5493dd75444695760b6dcc1253

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,6 @@
-def introduction(name, language) #hi
-    puts "Hi, my name is #{name} and i am learning to program in #{language}."
+def introduction(name) #hi
+    puts "Hi, my name is #{name}."
 end
-
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
